07171d34a496ffa132a8ecde0dde4b04804d0201
amine-bs <75427994+amine-bs@users.noreply.github.com>
2023-01-26 15:20:46 +0100

    Update install-extension.sh

--- install-extension.sh
+++ install-extension.sh
@@ -3,7 +3,6 @@ code-server --install-extension fauxpilot.vsix
 key="fauxpilot.server"
 value="$1:$2/v1/engines"
 settings="$HOME/.local/share/code-server/User"
-echo $settings
 jq --arg key "$key" --arg value "$value" --indent 4 '. += {($key): $value}'  $settings/settings.json > settings.json
 mv settings.json $settings
 rm fauxpilot.vsix
