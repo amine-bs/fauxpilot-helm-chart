21d29ea4eae27e1c46b185f02327e50ea671c577
amine-bs <75427994+amine-bs@users.noreply.github.com>
2023-01-26 15:31:39 +0100

    Update install-fauxpilot.sh

--- install-fauxpilot.sh
+++ install-fauxpilot.sh
@@ -1,8 +1,8 @@
-wget https://minio.lab.sspcloud.fr/mbenxsalha/diffusion/fauxpilot/fauxpilot.vsix 
-code-server --install-extension fauxpilot.vsix
+wget -O $HOME/fauxpilot.vsix https://minio.lab.sspcloud.fr/mbenxsalha/diffusion/fauxpilot/fauxpilot.vsix 
+code-server --install-extension $HOME/fauxpilot.vsix
 key="fauxpilot.server"
 value="$1:$2/v1/engines"
 settings="$HOME/.local/share/code-server/User"
-jq --arg key "$key" --arg value "$value" --indent 4 '. += {($key): $value}'  $settings/settings.json > settings.json
-mv settings.json $settings
-rm fauxpilot.vsix
+jq --arg key "$key" --arg value "$value" --indent 4 '. += {($key): $value}'  $settings/settings.json > $HOME/settings.json
+mv $HOME/settings.json $settings
+rm $HOME/fauxpilot.vsix
