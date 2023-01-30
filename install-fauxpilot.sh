820e9839d0feb08e6fdb65192eeafe4a8d734a80
amine-bs <75427994+amine-bs@users.noreply.github.com>
2023-01-30 10:02:04 +0100

    Update install-fauxpilot.sh

--- install-fauxpilot.sh
+++ install-fauxpilot.sh
@@ -3,6 +3,9 @@ code-server --install-extension $HOME/fauxpilot.vsix
 key="fauxpilot.server"
 value="$1:$2/v1/engines"
 settings="$HOME/.local/share/code-server/User"
-jq --arg key "$key" --arg value "$value" --indent 4 '. += {($key): $value}'  $settings/settings.json > $HOME/settings.json
+jq --arg key "$key" --arg value "$value" --indent 4 '. += {($key): $value}'  $settings/settings.json > $HOME/settings_1.json
+key="fauxpilot.enabled"
+value=$3
+jq --arg key "$key" --arg value "$value" --indent 4 '. += {($key): $value}'  $HOME/settings_1.json > $HOME/settings.json
 mv $HOME/settings.json $settings
 rm $HOME/fauxpilot.vsix
